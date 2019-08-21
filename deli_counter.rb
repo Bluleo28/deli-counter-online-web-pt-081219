496380844552865e415675e2e25aa0341eae3857
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-21 02:34:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,8 @@ def line(queue)
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
-    deli_queue.each_with_index do |name,index|
-      current_line << "#{index+1}. #{name}"
+    deli_queue.each_with_index(1) do |name,index|
+      current_line << "#{index}. #{name}"
     end
     puts current_line
   end 
