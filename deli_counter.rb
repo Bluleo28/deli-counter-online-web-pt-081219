a705f3547a39f943540db4a4bc515987be709d5e
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-21 02:24:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def line(queue)
+  if deli_queue.length == 0 
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    deli_queue.each_with_index do |name,index|
+      current_line << "#{index+1}. #{name}"
+    end
+    puts current_line
+  end 
+end
+
+
+
+
+def take_a_number(current_line, new_person)
+  if current_line << new_person
+    puts "Welcome, #{new_person}.You are number #{current_line.length} in line."
+  end
+end
+  
+  
+  
+  
+def now_serving(current_line)
+  if current_line.length == 0 
+    puts "Currently serving #{current_line[0]}."
+    current_line.shift
+  end
+end
