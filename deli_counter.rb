7814d6a297229acd8d6834b716a9aaafb4114c4a
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-21 03:16:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(deli_queue)
   else
     # current_line = "The line is currently:"
     message = "The line is currently:"
-    deli_queue.each_with_index do |name,index|
+    deli_queue.each_with_index do |name, index|
       message += " #{index +1}. #{name} "
     end
     puts "#{message}"
